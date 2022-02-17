355f53481beb1f3cd722b2967edd2b81050d4bd2
Aidan Sharp <enigma0fox@gmail.com>
2022-02-17 16:43:39 -0500

    pls do the stuff

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home() {
       <Head>
         <title>Siarune</title>
         <meta name={styles.Description} content="My stupid little site" />
-        {/* <link rel="icon" href="/hentie_solid.png" /> */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
