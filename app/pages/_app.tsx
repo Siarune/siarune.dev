2668e02f1f55de7fdcfdeb27d71e8be4c7535f3f
Aidan Sharp <sia@siarune.dev>
2022-05-08 19:12:31 -0400

    blitz rebase

---
+++ app/pages/_app.tsx
@@ -0,0 +1,58 @@
+import '../styles/global.sass'
+import base from '../styles/base.module.sass';
+import MetaBundle from '../core/components/MetaBundle'
+import CluckHUD from '../core/components/CluckHUD'
+import { ClerkProvider } from '@clerk/nextjs';
+
+import {
+  AppProps,
+  ErrorBoundary,
+  ErrorComponent,
+  AuthenticationError,
+  AuthorizationError,
+  ErrorFallbackProps,
+  useQueryErrorResetBoundary,
+} from "blitz"
+import LoginForm from "app/auth/components/LoginForm"
+
+export default function App({ Component, pageProps }: AppProps) {
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+    <ErrorBoundary
+      FallbackComponent={RootErrorFallback}
+      onReset={useQueryErrorResetBoundary().reset}
+    >
+      {getLayout(
+        <ClerkProvider>
+          <MetaBundle />
+
+          <main className={base.main}>
+
+            <CluckHUD />
+
+            <Component {...pageProps} />
+
+          </main>
+        </ClerkProvider>
+      )}
+    </ErrorBoundary>
+  )
+}
+
+function RootErrorFallback({ error, resetErrorBoundary }: ErrorFallbackProps) {
+  if (error instanceof AuthenticationError) {
+    return <LoginForm onSuccess={resetErrorBoundary} />
+  } else if (error instanceof AuthorizationError) {
+    return (
+      <ErrorComponent
+        statusCode={error.statusCode}
+        title="Sorry, you are not authorized to access this"
+      />
+    )
+  } else {
+    return (
+      <ErrorComponent statusCode={error.statusCode || 400} title={error.message || error.name} />
+    )
+  }
+}
---
+++ app/pages/projects/sauce.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import styles from '../../styles/sauce.module.sass'
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote } from 'next-mdx-remote';
+import { promises as fs } from 'fs'
+import path from 'path'
+
+export default function sauce({ source }: { source: any }) {
+	return (
+		<div className={styles.cont}>
+			<div className={styles.hentWrap}>
+				<Image
+					src="/hentieSolid.png"
+					alt="bart"
+					height="100"
+					width="100"
+					layout="fixed"
+					className={styles.portrait}
+				/>
+				<h1 className={styles.title}>Whatever This Place Is</h1>
+			</div>
+
+			<div className={styles.content}>
+				<MDXRemote {...source} components={components} className={styles.mdx} />
+			</div>
+		</div>
+	);
+}
+
+const components = {}
+
+export async function getStaticProps() {
+    const mdPath = await path.join(process.cwd(), 'public/markdown/sauce.mdx');
+    const rawMarkdown = await fs.readFile(mdPath, 'utf8');
+    const postMd = await serialize(rawMarkdown);
+
+    return { props: { source: postMd } }
+}
