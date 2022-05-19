9355cde66c0e0f2e01463e549e2e7df11be67ca6
Aidan <enigma0fox@gmail.com>
2022-05-19 13:21:57 -0400

    db init

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,36 +1,41 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/index.module.sass'
+import styles from "../styles/index.module.sass"
+import { Link, BlitzPage } from "blitz"
 
-export default function Home() {
+const Home: BlitzPage = () => {
 	return (
 		<div className={styles.cont}>
 			<div className={styles.grid}>
-
 				<div className={styles.card}>
-					<h2>
-						About Me
-					</h2>
-					
+					<h2>About Me</h2>
+
 					<p>
 						Hey, Sia here. <br />
-						Nice place, huh? It&apos;s taken quite a few redesigns, and this certainly won&apos;t be the last. :3
+						Nice place, huh? It&apos;s taken quite a few redesigns, and this certainly
+						won&apos;t be the last. :3
 						<br />
-						I&apos;m just a computer science student with a little too much free time. Why don&apos;t you check out the place while you&apos;re here?
+						I&apos;m just a computer science student with a little too much free time.
+						Why don&apos;t you check out the place while you&apos;re here?
 					</p>
 
 					<h4 className={styles.linksTitle}>Links</h4>
 
 					<ul className={styles.linkItems}>
-
 						<li className={styles.link1}>
-							<a href="https://github.com/Siarune" target="_blank" rel="noopener noreferrer">
+							<a
+								href="https://github.com/Siarune"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
 								Github
 							</a>
 						</li>
 
 						<li className={styles.link2}>
-							<a href="https://gitlab.com/Siarune" target="_blank" rel="noopener noreferrer">
+							<a
+								href="https://gitlab.com/Siarune"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
 								Gitlab
 							</a>
 						</li>
@@ -42,56 +47,50 @@ export default function Home() {
 						</li>
 
 						<li className={styles.link4}>
-							<a href="https://steamcommunity.com/profiles/76561199007571171/" target="_blank" rel="noopener noreferrer">
+							<a
+								href="https://steamcommunity.com/profiles/76561199007571171/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
 								Steam
 							</a>
 						</li>
-
 					</ul>
-
 				</div>
 
 				<div className={styles.card}>
 					<h2>
-						<Link href="/projects/sauce"><a>
-							This Website
-						</a></Link>
+						<Link href="/projects/sauce">
+							<a>This Website</a>
+						</Link>
 					</h2>
-					<h4>
-						A How and Why
-					</h4>
+					<h4>A How and Why</h4>
 				</div>
 
 				<div className={styles.card}>
 					<h2>
-						<Link href="/projects/poetry"><a>
-							Poetry is for Nerds
-						</a></Link>
+						<Link href="/projects/poetry">
+							<a>Poetry is for Nerds</a>
+						</Link>
 					</h2>
-					<h4>
-						...Wait, I&apos;m a nerd
-					</h4>
+					<h4>...Wait, I&apos;m a nerd</h4>
 				</div>
 
 				<div className={styles.card}>
 					<h2>
-						<Link href="/projects/maboibart"><a>
-							Bartholomew
-						</a></Link>
+						<Link href="/projects/maboibart">
+							<a>Bartholomew</a>
+						</Link>
 					</h2>
-					<h4>
-						He hates everything and it&apos;s great
-					</h4>
+					<h4>He hates everything and it&apos;s great</h4>
 				</div>
-
 			</div>
 
 			<footer className={styles.footer}>
-				<h3>
-					Everything Under Construction
-				</h3>
-
+				<h3>Everything Under Construction</h3>
 			</footer>
 		</div>
 	)
 }
+
+export default Home
--- app/pages/projects/maboibart.tsx
+++ app/pages/projects/maboibart.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../../styles/bartyboy.module.sass'
-import { serialize } from 'next-mdx-remote/serialize';
-import { MDXRemote } from 'next-mdx-remote';
-import { promises as fs } from 'fs'
-import path from 'path'
+import Image from "next/image"
+import Link from "next/link"
+import styles from "../../styles/bartyboy.module.sass"
+import { serialize } from "next-mdx-remote/serialize"
+import { MDXRemote } from "next-mdx-remote"
+import { promises as fs } from "fs"
+import path from "path"
 
 export default function bart({ source }: { source: any }) {
 	return (
@@ -25,15 +25,15 @@ export default function bart({ source }: { source: any }) {
 				<MDXRemote {...source} components={components} className={styles.mdx} />
 			</div>
 		</div>
-	);
+	)
 }
 
 const components = {}
 
 export async function getStaticProps() {
-	const mdPath = await path.join(process.cwd(), 'public/markdown/bart.mdx');
-	const rawMarkdown = await fs.readFile(mdPath, 'utf8');
-	const postMd = await serialize(rawMarkdown);
+	const mdPath = path.join(process.cwd(), "public/markdown/bart.mdx")
+	const rawMarkdown = await fs.readFile(mdPath, "utf8")
+	const postMd = await serialize(rawMarkdown)
 
 	return { props: { source: postMd } }
 }
